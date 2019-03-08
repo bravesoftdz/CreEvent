ed5e865d16544155cb4cf66577b49f47db363ad7
walwalwalides <walwalwalides@gmail.com>
2019-03-08 18:24:36 +0100

    Start

---
+++ CreateEvent.dpr
@@ -0,0 +1,18 @@
+program CreateEvent;
+
+uses
+  Forms,
+  Main in 'Form\Main.pas' {frmMain} ,
+  uEvent in 'Unit\uEvent.pas',
+  Module in 'Module\Module.pas' {DMModule: TDataModule};
+
+{$R *.res}
+
+begin
+  Application.Initialize;
+  Application.Title := 'CreateEvent';
+  Application.CreateForm(TDMModule, DMModule);
+  Application.CreateForm(TfrmMain, frmMain);
+  Application.Run;
+
+end.
